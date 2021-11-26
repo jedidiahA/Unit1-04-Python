ef781afed0eea3cabcb802e683bd05d2c85777e3
Ubuntu <ubuntu@ip-172-31-90-30.ec2.internal>
2021-11-26 01:51:40 +0000

    I made this program to calculate area and perimeter of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created By: Jedidiah
+# Date: Nov. 25, 2021
+# This program calculates the area and perimeter of a rectangle
+#   with dimensions 6cm x 4cm
+
+
+def main():
+    # This function calculates the area and perimeter of a rectangle
+    print("if a rectangle has the dimensions:")
+    print("6cm x 4cm")
+    print(6*4)
+    print("")
+    print("Area is {}cm^2".format(6*4))
+    print("perimeter is {}".format(2*10))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Jedidiah
+# Date: Nov. 25, 2021
+# This program does basic math.
+
+
+def main():
+    # This function does basic math.
+    print(56%5)
+    print(5+4-1)
+    print(40/2**3)
+
+
+if __name__ == "__main__":
+    main()
